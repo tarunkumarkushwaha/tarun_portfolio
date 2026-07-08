b2d551915c6e64dbc697c7fc13b0fac9df0a49de
tarun <tkk.tarunkumar02@gmail.com>
2026-07-08 12:56:35 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = forwardRef((prop, ref) => {
           </p>
           <div className="mt-10 grid grid-cols-2 md:grid-cols-3 gap-6 pt-10 border-t border-slate-500/20">
             <div className="flex flex-col">
-              <span className="text-blue-500 zoom font-bold text-2xl">3+</span>
+              <span className="text-blue-500 zoom font-bold text-2xl">2+</span>
               <span
                 className={`text-xs zoom uppercase tracking-widest font-semibold ${darkmode ? "text-slate-500" : "text-slate-400"}`}
               >
@@ -52,7 +52,7 @@ const About = forwardRef((prop, ref) => {
               </span>
             </div>
             <div className="flex flex-col">
-              <span className="text-emerald-500 zoom font-bold text-2xl">15+</span>
+              <span className="text-emerald-500 zoom font-bold text-2xl">5+</span>
               <span
                 className={`text-xs zoom uppercase tracking-widest font-semibold ${darkmode ? "text-slate-500" : "text-slate-400"}`}
               >
@@ -60,7 +60,7 @@ const About = forwardRef((prop, ref) => {
               </span>
             </div>
             <div className="flex flex-col">
-              <span className="text-purple-500 font-bold text-2xl">8y</span>
+              <span className="text-purple-500 font-bold text-2xl">3y</span>
               <span
                 className={`text-xs uppercase tracking-widest font-semibold ${darkmode ? "text-slate-500" : "text-slate-400"}`}
               >
